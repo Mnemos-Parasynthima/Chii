2ea81dec639e3898408a188f777a19ca838e8493
MnemosParasynth <jorgegarcia_tx@outlook.com>
2020-12-21 18:56:26 +0000

    Added hiss and souldreg command.

---
+++ commands/hiss.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: 'hiss',
+  description: 'Hisses',
+  execute(message) {
+    const taggedUser = message.mentions.users.first();
+
+    if (!taggedUser) {
+      message.channel.send('Huh?');
+    }
+
+    if (taggedUser) {
+      // this gets the member from the user
+      const user = message.guild.member(taggedUser);
+      if (user) {
+        message.channel.send(`${user}, HIISSSSS!!!`);
+      }
+    }
+  }
+}
---
+++ commands/souldreg.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'souldreg',
+  description: 'Responds to the presence of SoulDregs',
+  execute(message) {
+    message.reply('SoulDregs? Where? I\'ll destroy them right now-nya!');
+  }
+}
