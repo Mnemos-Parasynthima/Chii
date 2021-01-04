2c6023c50f7583ee5fa723ebb4f4e9be5562bd73
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-04 15:55:01 +0000

    Added more fates, changed the way it returns a rando fate. Updated package.js.

--- commands/fate.js
+++ commands/fate.js
@@ -3,17 +3,10 @@ module.exports = {
   description: 'Gives the fate',
   ownerOnly: true,
   execute(message) {
-    let fates = ['By the power of a SoulWorker, yes', 'To a SoulDreg, I say no', 'Be it true', 'idk', 'You\'re life will change'], i = 0; //Array of possible fates
+    let fates = ['By the power of a SoulWorker, yes', 'To a SoulDreg, I say no', 'Be it true', 'idk', 'Your life will change', 'It seems so']; //Array of possible fates
 
-    i++;
-    console.log(fates.length);
-    console.log(i);
-    console.log(i % fates.length);
-    console.log(fates[i % fates.length]);
-    //const fate = fates[i++ % fates.length];
-    //console.log(fate);
-    
-    //message.reply(`${fate}`);
+    const output = Math.floor(Math.random()*fates.length);
+    message.reply(`${fates[output]}`);
     
   }
 };
