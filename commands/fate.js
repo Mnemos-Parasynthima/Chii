cefa9bb6effa8428ecf6ae63eef798957ad03cc9
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-14 17:54:47 +0000

    Added to text arrays

--- commands/fate.js
+++ commands/fate.js
@@ -1,12 +1,15 @@
 module.exports = {
   name: 'fate',
   description: 'Gives the fate',
+  aliases: ['f'],
   ownerOnly: true,
   execute(message) {
-    let fates = ['By the power of a SoulWorker, yes', 'To a SoulDreg, I say no', 'Be it true', 'idk', 'Your life will change', 'It seems so']; //Array of possible fates
+    let fates = ['By the power of a SoulWorker, yes', 'To a SoulDreg, I say no', 'Be it true', 
+      'idk', 'Your life will change', 'It seems so', ''
+    ]; //Array of possible fates
 
     const output = Math.floor(Math.random()*fates.length);
-    message.reply(`${fates[output]}`);
-    
+    message.reply(fates[output]);
+
   }
 };
--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,9 @@ module.exports = {
   name: 'say',
   description: 'Returns a statement',
   execute(message) {
-    let replies = ['Nya', 'I hope you are doing well', 'I am the best SoulWorker', 'My Master is the best programmer'];
+    let replies = ['Nya', 'I hope you are doing well', 'I am the best SoulWorker', 'My Master is the best programmer', 
+      'I love you, nya'
+    ];
     const reply = Math.floor(Math.random() * replies.length);
     message.channel.send(replies[reply]);
   }
