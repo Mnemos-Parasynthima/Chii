7658f2d876b61abf5268cd7987ec84ec176de06d
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-16 07:33:24 +0000

    Added batch file to install all necessary packages

---
+++ installpckgs.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+TITLE Installing necessary packages
+ECHO This will take a while
+npm i discord.js && npm i discord.js-commando && npm i request
+ECHO All necessary packages have been installed
+PAUSE
