c12cb28a7df42d508221d296c3b50c6e0e39c81d
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-16 07:46:53 +0000

    Added uninstalling packages batch file

---
+++ uninstallpckgs.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+TITLE Uninstalling packages
+ECHO This will take a while
+npm r discord.js && npm r discord.js-commando && npm r request
+ECHO All necessary packages have been installed
+PAUSE
