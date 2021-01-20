df6f2588a59dbcacceb7e205eee032e93008e94b
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-20 18:38:40 +0000

    Updated batch files

--- installpckgs.bat
+++ installpckgs.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 TITLE Installing necessary packages
 ECHO This will take a while
-npm i discord.js && npm i discord.js-commando && npm i request
+npm i discord.js && npm i node-fetch && npm i random-puppy && npm i request
 ECHO All necessary packages have been installed
 PAUSE
--- uninstallpckgs.bat
+++ uninstallpckgs.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 TITLE Uninstalling packages
 ECHO This will take a while
-npm r discord.js && npm r discord.js-commando && npm r request
+npm r discord.js && npm r node-fetch && npm r random-puppy && npm r request
 ECHO All necessary packages have been installed
 PAUSE
