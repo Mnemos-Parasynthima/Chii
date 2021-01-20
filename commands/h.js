63e71a33ee62bf5c3866b80784614e5d4b7754d5
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-20 20:06:23 +0000

    Added to useless command

---
+++ commands/h.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy');
+
+module.exports = {
+  name: 'h',
+  description: 'No description necessary',
+  aliases: ['nh'],
+  execute(message) {
+    const owner = process.env.ownerId;
+    if (message.author.id === owner && message.channel.nsfw === true) {
+      const subreddits = ['animemes'];
+      const subreddit = Math.floor(Math.random() * subreddits.length);
+      const source = subreddits[subreddit];
+      randomPuppy(source) // Name of subreddits
+        .then(url => {
+          console.log(`${url}; Source: ${source}`);
+          const embed = new Discord.MessageEmbed()
+            .setTitle('NSFW stuff')
+            .setColor('#ff0000')
+            .setDescription(`${url}`)
+            .setImage(url)
+            .setFooter(`From '${source}' subreddit`, message.author.displayAvatarURL({ size: 32 }))
+            .setTimestamp()
+          return message.channel.send(embed);
+        })
+        .catch(error => {
+          console.error(error);
+          //message.channel.send('Error! Could not find image-nya! Try again');
+        });
+    } else if (message.author.id !== owner) { message.reply('Command not allowed');
+    } else if (message.channel.nsfw === false) { message.reply('Illegal!');
+    } else { return; }
+  }
+};
--- commands/useless.js
+++ commands/useless.js
@@ -5,13 +5,16 @@ module.exports = {
   aliases: [''],
 	execute(message) {
 		const taggedUser = message.mentions.users.first();
+    const owner = process.env.ownerId;
 		
     if (!taggedUser) { 
       message.channel.send('Everyone\'s useless! Except for my Master-nya');
     }
 
-    if (taggedUser) {
+    if (taggedUser && taggedUser.user.id !== owner) {
 	    message.channel.send(`Useless ${taggedUser}-nya!`);
+    } else {
+      message.reply('No u!');
     }
 	}
 };
