25acac66b2db5ebc17b014f311758823d2fc5f8f
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-22 06:06:01 +0000

    Updated say and package.json version

--- commands/say.js
+++ commands/say.js
@@ -4,7 +4,7 @@ module.exports = {
   aliases: ['say'],
   execute(message) {
     let replies = ['Nya', 'I hope you are doing well', 'I am the best SoulWorker', 'My Master is the best programmer', 
-      'I love you, nya', 'I\'m sad', 'I wuv my Master	(⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄)', 'I\'m proud', '♡＾▽＾♡', '┐( ˘_˘ )┌'
+      'I love you, nya', 'I wuv my Master	(⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄)', 'I\'m proud', '♡＾▽＾♡', '┐( ˘_˘ )┌'
     ];
     const reply = Math.floor(Math.random() * replies.length);
     message.channel.send(replies[reply]);
