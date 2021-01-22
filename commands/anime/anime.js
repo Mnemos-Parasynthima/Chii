e202707a35aa38a8f34648c0c23b0c3aea4e77f1
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-22 19:56:45 +0000

    Updated to Commando

--- commands/anime/anime.js
+++ commands/anime/anime.js
@@ -1,34 +1,47 @@
 const Discord = require('discord.js');
+const { Command } = require('discord.js-commando');
 const randomPuppy = require('random-puppy');
 /*
 ****TODO****
 Incorporate better subreddit image get
 */
-module.exports = {
-  name: 'anime',
-  description: 'NYA!! [Warning: repetitive images]',
-  aliases: ['awwnime', 'moe'],
-  execute(message) {
-    const subreddits = ['Animewallpaper', 'Re_Zero', 'OneTrueEmilia', 'awwnime']; // TODO: Add more subreddits
-    const subreddit = Math.floor(Math.random() * subreddits.length);
-    const source = subreddits[subreddit];
-    randomPuppy(source) // Name of subreddits
+module.exports = class AnimeCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'anime',
+      aliases: ['awwnime', 'moe'], // Change aliases
+      group: 'anime',
+      memberName: 'anime',
+      description: 'Animenya! [Note: Repetitive images]',
+      throttling: {
+        usages: 3,
+        duration: 3,
+      }
+    });
+  }
+
+  run(msg) {
+    // Array of subreddits to fetch data from
+    const subreddits = ['Animewallpaer', 'Re_Zero', 'OneTrueEmilia', 'awwnime']; // TODO: Add more subreddits
+    const subreddit = Math.floor(Math.random() * subreddits.length); // Random nuber
+    const source = subreddits[subreddit]; // Gets a random subreddit from array
+
+    randomPuppy(source) // Name of subreddit between parenth.
       .then(url => {
-        //if (url === undefined) return
-        //if (url === 'https://i.imgur.com/removed.png') return
-        console.log(`${url}; Source: ${source}`);
+        console.log(`${url}; Source: ${source}`); // For retrieval info
         const embed = new Discord.MessageEmbed()
           .setTitle('Anime Pictures!')
           .setColor('#ff0000')
           .setDescription(`${url}`)
           .setImage(url)
-          .setFooter(`Request by: ${message.author.username} | From '${source}' subreddit`, message.author.displayAvatarURL({ size: 32 }))
+          .setFooter(`Request by: ${msg.author.username} | From '${source}' subreddit`, msg.author.displayAvatarURL({ size: 32 }))
           .setTimestamp()
-	      return message.channel.send(embed);
+
+        return msg.embed(embed);
       })
       .catch(error => {
         console.error(error);
-        message.channel.send('Error! Could not find image-nya! Try again');
+        msg.say('Error!');
       });
   }
 };
--- commands/anime/nekos.js
+++ commands/anime/nekos.js
@@ -1,18 +1,30 @@
 const Discord = require('discord.js');
+const { Commando } = require('discord.js-commando');
 const fetch = require('node-fetch');
 
-module.exports = {
-  name: 'nekos',
-  description: 'NYA!!',
-  aliases: ['nya', 'catgirls', 'neko'],
-  async execute(message) {
+module.exports = class NekosCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'nekos',
+      aliases: ['nya', 'catgirls', 'neko'],
+      group: 'anime',
+      memberName: 'nekos',
+      description: 'NYA!!!',
+      throttling: {
+        usages: 3,
+        duration: 3,
+      }
+    });
+  }
+  run(msg) {
+    // Gets image data from api and sends as json
 	  const { url } = await fetch("https://nekos.life/api/v2/img/neko").then((res) => res.json());
     const embed = new Discord.MessageEmbed()
       .setTitle('Nekos')
       .setColor('#ff0000')
       .setImage(url)
-      .setFooter(`Request by: ${message.author.username} | Powered by nekos.life`, message.author.displayAvatarURL({ size: 32 }))
+      .setFooter(`Request by: ${msg.author.username} | Powered by nekos.life`, msg.author.displayAvatarURL({ size: 32 }))
       .setTimestamp()
-	  message.channel.send(embed);
+	  msg.embed(embed);
   }
-}
+};
