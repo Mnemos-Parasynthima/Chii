8675d4d091257f97440173007332fa233b602c4e
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-28 06:09:32 +0000

    Updated

--- installpckgs.bat
+++ installpckgs.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 TITLE Installing necessary packages
 ECHO This will take a while...
-npm i cheerio & npm i discord-youtube-api & npm i discord.js & npm i https://github.com/discordjs/Commando & npm i node-fetch & npm i random-puppy & npm i request
+npm i @discordjs/opus & npm i discord.js & npm i https://github.com/discordjs/Commando & npm i ffmpeg-static & npm i node-fetch & npm i random-puppy & npm i request & npm i yt-search & npm i ytdl-core-discord
 ECHO All necessary packages have been installed
 PAUSE
--- uninstallpckgs.bat
+++ uninstallpckgs.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 TITLE Uninstalling necessary packages
 ECHO This will take a while...
-npm r cheerio & npm r discord-youtube-api & npm r discord.js & npm r https://github.com/discordjs/Commando & npm r node-fetch & npm r random-puppy & npm r request
+npm r @discordjs/opus & npm r discord.js & npm r https://github.com/discordjs/Commando & npm r ffmpeg-static & npm r node-fetch & npm r random-puppy & npm r request & npm r yt-search & npm r ytdl-core-discord
 ECHO All packages have been uninstalled
 PAUSE
