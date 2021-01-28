648c10f5a8724a615d7c3e750898ceafdcea132d
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-28 05:22:07 +0000

    Added pause and resume commands

---
+++ commands/music/pause.js
@@ -0,0 +1,48 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class PauseCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'pause',
+      aliases: ['ps'],
+      group: 'music',
+      memberName: 'pause',
+      description: 'Pauses the current music/video.',
+      guildOnly: true,
+      throttling: {
+        usages: 3,
+        duration: 5,
+      }
+    });
+  }
+
+  async run(msg) {
+    const { channel } = msg.member.voice;
+    const serverQueue = this.client.queue.get(msg.guild.id);
+    const embed = new MessageEmbed()
+      .setColor('#ff0000')
+      .setDescription('⏸ **PAUSED**')
+
+    try {
+      if (!channel) {
+        msg.say('Nyoin a voice chyannel!')
+        return;
+      }
+      if (msg.guild.me.voice.channel !== msg.member.voice.channel) {
+        msg.say('Be with me!');
+        return;
+      }
+
+      if (serverQueue && serverQueue.playing) {
+        serverQueue.playing = false;
+        serverQueue.connection.dispatcher.pause(true);
+        msg.embed(embed);
+      } else { msg.say('Nyothing\'s playing!'); }
+    } catch {
+      console.log('Entered catch');
+      serverQueue.connection.dispatcher.end();
+      await channel.leave();
+    }
+  }
+}
