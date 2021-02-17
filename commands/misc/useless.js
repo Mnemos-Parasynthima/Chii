4d35b6dc4bc5636f2099a426c0aea8e849bf985f
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-02-17 21:13:38 +0000

    Fixed id property undefined

--- commands/misc/useless.js
+++ commands/misc/useless.js
@@ -24,19 +24,20 @@ module.exports = class UselessCommand extends Command {
 		const taggedUser = msg.mentions.users.first();
     const owner = process.env.ownerId;
 		
-    if (taggedUser.id === process.env.selfId ) return msg.reply('I\'m nyever useless, you are!');
 
     if (!taggedUser) { // If no tag
       embed.setDescription('Everyone\'s useless! Except for my Master-nya');
-      msg.say(embed);
+      return msg.say(embed);
     }
+
+    if (taggedUser.id === msg.client.user.id ) return msg.reply('I\'m nyever useless, you are!');
 
     if (taggedUser && taggedUser.id !== owner) { // If tag and tag is not owner
       const i = Math.floor(Math.random() * uselessAqua.length);
       embed.setDescription(`Useless ${taggedUser}-nya! Just like Aqua`).setImage(uselessAqua[i]);
-	    msg.embed(embed);
+	    return msg.embed(embed);
     } else if (taggedUser && taggedUser.id === owner) { // If tag and tag is owner
-      msg.reply('No u!');
+      return msg.reply('No u!');
     }
   }
 };
