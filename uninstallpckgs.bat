4060784462b9268175fd7ccbd1d59ede51616d46
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-02-19 20:49:32 +0000

    Removed batch files

--- uninstallpckgs.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-TITLE Uninstalling necessary packages
-ECHO This will take a while...
-npm r @discordjs/opus & npm r booru & npm r discord.js & npm r https://github.com/discordjs/Commando & npm r ffmpeg-static & npm r node-fetch & npm r random-puppy & npm r request & npm r yt-search & npm r ytdl-core-discord
-ECHO All packages have been uninstalled
-PAUSE
