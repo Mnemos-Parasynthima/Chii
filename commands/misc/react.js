23d3e2acbccd0f9556aa06d85db68eb41a1c2062
MnemosParasynth <66635951+Mnemos-Parasynthima@users.noreply.github.com>
2022-06-29 17:31:22 +0000

    Updated reactions

--- commands/misc/react.js
+++ commands/misc/react.js
@@ -7,7 +7,7 @@ module.exports = class ReactCommand extends Command {
       aliases: ['rct', 'reaccionar', 'hanno'],
       group: 'misc',
       memberName: 'react',
-      description: `React! using \`shrug\`, \`arua\`, \`ephnel\`, \`yorka\`, \`sip\`, or \`think\`, \`happy1\`, \`happy2\`, \`serious\`, \`satisproud\`, \`sad\`, \`annoyger\`, \`wut\`, \`no\``,
+      description: `React! using \`think\`, \`sip\`, \`yorka\`, \`ephnel\`, \`arua\`, \`shrug\`, \`sad\`, \`proud\`, \`happy\`, \`denied\`, \`wut\`, \`yes\`, \`lazy\`, \`baka\`, \`angry\`, \`mad\``,
       guildOnly: true,
       format: '<reaction>',
       throttling: {
@@ -27,62 +27,70 @@ module.exports = class ReactCommand extends Command {
   async run(msg, { args }) {
     try {
       const sendReaction = (msg, args) => {
-        //cons happys = 
         switch (args) {
-          case 'shrug':
-            const shrug = this.client.emojis.cache.find(emoji => emoji.name === "shrug");
-            msg.say(`<:${shrug.name}:${shrug.id}>`);
-            break;
-          case 'arua':
-            const arua = this.client.emojis.cache.find(emoji => emoji.name === "arua");
-            msg.say(`<:${arua.name}:${arua.id}>`);
-            break;
-          case 'ephnel':
-            const ephnel = this.client.emojis.cache.find(emoji => emoji.name === "ephnel");
-            msg.say(`<:${ephnel.name}:${ephnel.id}>`);
-            break;
-          case 'yorka':
-            const yorka = this.client.emojis.cache.find(emoji => emoji.name === "yorka");
-            msg.say(`<:${yorka.name}:${yorka.id}>`);
+          case 'think':
+            const think = this.client.emojis.cache.find(emoji => emoji.name === "think");
+            msg.say(`<:${think.name}:${think.id}>`);
             break;
           case 'sip':
             const sip = this.client.emojis.cache.find(emoji => emoji.name === "sip");
             msg.say(`<:${sip.name}:${sip.id}>`);
             break;
-          case 'think':
-            const think = this.client.emojis.cache.find(emoji => emoji.name === "think");
-            msg.say(`<:${think.name}:${think.id}>`);
+          case 'yorka':
+            const yorka = this.client.emojis.cache.find(emoji => emoji.name === "yorka");
+            msg.say(`<:${yorka.name}:${yorka.id}>`);
             break;
-          case 'happy1':
-            const happy1 = this.client.emojis.cache.find(emoji => emoji.name === "happy1");
-            msg.say(`<:${happy1.name}:${happy1.id}>`);
+          case 'ephnel':
+            const ephnel = this.client.emojis.cache.find(emoji => emoji.name === "ephnel");
+            msg.say(`<:${ephnel.name}:${ephnel.id}>`);
             break;
-          case 'happy2':
-            const happy2 = this.client.emojis.cache.find(emoji => emoji.name === "happy2");
-            msg.say(`<:${happy2.name}:${happy2.id}>`);
+          case 'arua':
+            const arua = this.client.emojis.cache.find(emoji => emoji.name === "arua");
+            msg.say(`<:${arua.name}:${arua.id}>`);
             break;
-          case 'serious':
-            const serious = this.client.emojis.cache.find(emoji => emoji.name === "serious");
-            msg.say(`<:${serious.name}:${serious.id}>`);
-            break;
-          case 'satisproud':
-            const satisproud = this.client.emojis.cache.find(emoji => emoji.name === "satisproud");
-            msg.say(`<:${satisproud.name}:${satisproud.id}>`);
+          case 'shrug':
+            const shrug = this.client.emojis.cache.find(emoji => emoji.name === "shrug");
+            msg.say(`<:${shrug.name}:${shrug.id}>`);
             break;
           case 'sad':
             const sad = this.client.emojis.cache.find(emoji => emoji.name === "sad");
             msg.say(`<:${sad.name}:${sad.id}>`);
             break;
-          case 'annoyger':
-            const annoyger = this.client.emojis.cache.find(emoji => emoji.name === "annoyger");
-            msg.say(`<:${annoyger.name}:${annoyger.id}>`);
-          case 'wut':
-            const wut = this.client.emojis.cache.find(emoji => emoji.name === "wut");
-            msg.say(`<:${wut.name}:${wut.id}>`);
+          case 'proud':
+            const proud = this.client.emojis.cache.find(emoji => emoji.name === "proud");
+            msg.say(`<:${proud.name}:${proud.id}>`);
+            break;
+          case 'happy':
+            const happy = this.client.emojis.cache.find(emoji => emoji.name === "happy");
+            msg.say(`<:${happy.name}:${happy.id}>`);
             break;
           case 'denied':
             const denied = this.client.emojis.cache.find(emoji => emoji.name === "denied");
             msg.say(`<:${denied.name}:${denied.id}>`);
+            break;
+          case 'wut':
+            const wut = this.client.emojis.cache.find(emoji => emoji.name === "wut");
+            msg.say(`<:${wut.name}:${wut.id}>`);
+            break;
+          case 'yes':
+            const yes = this.client.emojis.cache.find(emoji => emoji.name === "yes");
+            msg.say(`<:${yes.name}:${yes.id}>`);
+            break;
+          case 'lazy':
+            const lazy = this.client.emojis.cache.find(emoji => emoji.name === "lazy");
+            msg.say(`<:${lazy.name}:${lazy.id}>`);
+            break;
+          case 'baka':
+            const bakathink = this.client.emojis.cache.find(emoji => emoji.name === "baka");
+            msg.say(`<:${baka.name}:${baka.id}>`);
+            break;
+          case 'angry':
+            const angry = this.client.emojis.cache.find(emoji => emoji.name === "angry");
+            msg.say(`<:${angry.name}:${angry.id}>`);
+            break;
+          case 'mad':
+            const mad = this.client.emojis.cache.find(emoji => emoji.name === "mad");
+            msg.say(`<:${mad.name}:${mad.id}>`);
             break;
           default:
             msg.reply('That\'s nyot a reaction! Type \`chii help react\` to knyow the reactions!');
